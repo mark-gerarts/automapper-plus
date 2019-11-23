361c14bbd2f2c763f1693f874ba786990e12c39a
Mark Gerarts <mark.gerarts@gmail.com>
2019-11-23 23:22:16 +0100

    Add more data types to the DataType enum

--- src/DataType.php
+++ src/DataType.php
@@ -10,9 +10,16 @@ namespace AutoMapperPlus;
 final class DataType
 {
     public const ARRAY = 'array';
+    public const INTEGER = 'integer';
+    public const FLOAT = 'float';
+    public const STRING = 'string';
 
     public static function isDataType(string $type): bool
     {
-        return \in_array($type, [self::ARRAY], true);
+        return \in_array(
+            $type,
+            [self::ARRAY, self::INTEGER, self::FLOAT, self::STRING],
+            true
+        );
     }
 }
