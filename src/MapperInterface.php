f760225b476950d4a160bdc0e93ec71a02bacd88
Alessandro Lai <alessandro.lai85@gmail.com>
2021-03-12 11:33:14 +0100

    Add template annotation to MapperInterface

--- src/MapperInterface.php
+++ src/MapperInterface.php
@@ -15,9 +15,11 @@ interface MapperInterface
      * Maps an object to an instance of class $to, provided a mapping is
      * configured.
      *
+     * @template T of object
      * @param array|object $source
      *   The source object.
      * @param string $targetClass
+     * @psalm-param class-string<T> $targetClass
      *   The target classname.
      * @param array $context
      *   An arbitrary array of values that will be passed to supporting
@@ -26,6 +28,7 @@ interface MapperInterface
      *   This is not explicitly required on the interface yet to preserve
      *   backwards compatibility, but will be added in version 2.0.
      * @return mixed
+     * @psalm-return T
      *   An instance of class $to.
      * @throws UnregisteredMappingException
      */
@@ -34,13 +37,16 @@ interface MapperInterface
     /**
      * Maps properties of object $from to an existing object $to.
      *
+     * @template T of object
      * @param array|object $source
      *   The source object.
      * @param object $destination
+     * @psalm-param T $destination
      *   The target object.
      * @param array $context
      *   See MapperInterface::map()
      * @return mixed
+     * @psalm-return T
      *   $to, with properties copied from $from.
      * @throws UnregisteredMappingException
      */
